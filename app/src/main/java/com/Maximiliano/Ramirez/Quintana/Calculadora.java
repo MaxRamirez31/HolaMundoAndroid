e08720abc450f0cbe5a29cfa019de5cf4f971481
Ramírez Quintana Maximiliano Baruc <89669300+MaxRamirez31@users.noreply.github.com>
2021-11-05 22:29:10 -0600

    Tercera versión(cambio con la logica de la Calculadora)

---
+++ app/src/main/java/com/Maximiliano/Ramirez/Quintana/Calculadora.java
@@ -0,0 +1,22 @@
+package com.Maximiliano.Ramirez.Quintana;
+
+import java.io.Serializable;
+
+public class Calculadora implements Serializable {
+
+    public void suma(){
+
+    }
+    public void resta(){
+
+    }
+    public void multiplicación(){
+
+    }
+    public void división(){
+
+    }
+
+
+}
+
