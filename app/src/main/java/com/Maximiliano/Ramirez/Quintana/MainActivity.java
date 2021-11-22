8667457f4760e6c9daf2ae35c4046bd33caa8c8f
Ramírez Quintana Maximiliano Baruc <89669300+MaxRamirez31@users.noreply.github.com>
2021-11-21 20:54:13 -0600

    Se agregó método, actividad para la suma

--- app/src/main/java/com/Maximiliano/Ramirez/Quintana/MainActivity.java
+++ app/src/main/java/com/Maximiliano/Ramirez/Quintana/MainActivity.java
@@ -2,13 +2,39 @@ package com.Maximiliano.Ramirez.Quintana;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void suma(View miView)
+    { }
+
+
+    private Button Button_suma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button_suma = findViewById(R.id.Button_suma);
+
+        Button_suma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirSuma();
+            }
+        });
+
+
     }
+
+    private void abrirSuma() {
+        Intent intent = new Intent(this , SumaActivity.class);
+        startActivity(intent);
+    }
+
+
 }
