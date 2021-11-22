f6413a04ca274e0b27d44cdd359fe74ad86a8d42
Ramírez Quintana Maximiliano Baruc <89669300+MaxRamirez31@users.noreply.github.com>
2021-11-22 00:08:44 -0600

    Se agregaron métodos y actividades para la Resta, Multiplicación y División

--- app/src/main/java/com/Maximiliano/Ramirez/Quintana/MainActivity.java
+++ app/src/main/java/com/Maximiliano/Ramirez/Quintana/MainActivity.java
@@ -11,15 +11,26 @@ public class MainActivity extends AppCompatActivity {
 
     public void suma(View miView)
     { }
-
+    public void resta(View miView)
+    { }
+    public void multiplicacion(View miView)
+    { }
+    public void division(View miView)
+    { }
 
     private Button Button_suma;
+    private Button Button_resta;
+    private Button Button_division;
+    private Button Button_multiplicacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button_suma = findViewById(R.id.Button_suma);
+        Button_resta = findViewById(R.id.Button_resta);
+        Button_multiplicacion = findViewById(R.id.Button_multiplicacion);
+        Button_division = findViewById(R.id.Button_division);
 
         Button_suma.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +39,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button_resta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirResta();
+            }
+        });
+
+        Button_multiplicacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirMultiplicacion();
+            }
+        });
+
+        Button_division.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirDivision();
+            }
+        });
 
     }
 
@@ -36,5 +67,18 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private void abrirResta() {
+        Intent intent = new Intent(this , RestaActivity.class);
+        startActivity(intent);
+    }
 
+    private void abrirMultiplicacion() {
+        Intent intent = new Intent(this , multiplicacionActivity.class);
+        startActivity(intent);
+    }
+
+    private void abrirDivision() {
+        Intent intent = new Intent(this , divisionActivity.class);
+        startActivity(intent);
+    }
 }
