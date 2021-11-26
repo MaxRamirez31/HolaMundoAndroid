d8205418d3dda9dd73ef4612f9cae542728067ef
Ramírez Quintana Maximiliano Baruc <89669300+MaxRamirez31@users.noreply.github.com>
2021-11-26 01:49:50 -0600

    Se agregó metódo, actividad para mi operación

--- app/src/main/java/com/Maximiliano/Ramirez/Quintana/MainActivity.java
+++ app/src/main/java/com/Maximiliano/Ramirez/Quintana/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private Button Button_resta;
     private Button Button_division;
     private Button Button_multiplicacion;
+    private Button button_operacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         Button_resta = findViewById(R.id.Button_resta);
         Button_multiplicacion = findViewById(R.id.Button_multiplicacion);
         Button_division = findViewById(R.id.Button_division);
+        button_operacion = findViewById(R.id.button_operacion);
 
         Button_suma.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,6 +62,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        button_operacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirOperacion();
+            }
+        });
+
+    }
+
+    private void abrirOperacion() {
+        Intent intent = new Intent(this , operacion.class);
+        startActivity(intent);
     }
 
     private void abrirSuma() {
